3af5ccbbbfa04798c7f153c511279920fceb7035
Siriwat <hisiriwat@gmail.com>
2020-04-20 17:22:42 +0700

    Add font.

--- flutter_layout_widgets/lib/main.dart
+++ flutter_layout_widgets/lib/main.dart
@@ -9,29 +9,31 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white,
         body: SafeArea(
-          child: Row /*or Column*/(
-          mainAxisAlignment: MainAxisAlignment.start,
-          //mainAxisAlignment: MainAxisAlignment.center,
-          //mainAxisAlignment: MainAxisAlignment.end,
-
-          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          // mainAxisAlignment: MainAxisAlignment.spaceAround,
-
-          //crossAxisAlignment: CrossAxisAlignment.start,
-          //crossAxisAlignment: CrossAxisAlignment.center,
-            //crossAxisAlignment: CrossAxisAlignment.end,
-            //crossAxisAlignment: CrossAxisAlignment.stretch,
-
-            //mainAxisSize: MainAxisSize.max,
-            //mainAxisSize: MainAxisSize.min,
-
-          children: <Widget>[
-            Icon(Icons.star, size: 50),
-            Icon(Icons.star, size: 50),
-            Icon(Icons.star, size: 50),
-          ],
-        ),
+          child:Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/images/siriwat.jpg'),
+              ),
+              Text(
+                'Hi Siriwat',
+                style: TextStyle(
+                  fontFamily: 'Sriracha',
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+//                  color: Colors.pinkAccent
+                ),
+              ),
+              Text(
+                'โปรแกรมเมอร์',
+                style: TextStyle(
+                  fontFamily: 'Sriracha',
+                  fontSize: 20.0,
+                  color: Colors.red[300]
+                ),
+              ),
+            ],
+          )
         ),
       ),
     );
